2bac3e844c1428a85dc1264e7e4d61ea45224a72
GasAR <A.Gasymova@bssys.com>
2019-09-09 16:53:14 +0400

    ещё чут чут

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ class App extends React.Component {
 			<input type="text" className="input-list-item" onChange={this.handleName}/>
 			<button className="add-info-button button">V</button>
 			</div>
-			<div className="list-item container">Поиграть в приставку</div>
-            <div className="list-item container">Помыть пол</div>
-            <div className="list-item container">Сходить в магазин</div>
-            <div className="list-item container">Почесать котэ</div>
-            <div className="list-item container">Убрать говно</div>
-            <div className="list-item container">Пнуть пса</div>
+			<div className="list-item container">Поиграть в приставку <button className="del-info-button button">X</button></div>
+            <div className="list-item container">Помыть пол <button className="del-info-button button">X</button></div>
+            <div className="list-item container">Сходить в магазин <button className="del-info-button button">X</button></div>
+            <div className="list-item container">Почесать котэ <button className="del-info-button button">X</button></div>
+            <div className="list-item container">Убрать говно <button className="del-info-button button">X</button></div>
+            <div className="list-item container">Пнуть пса <button className="del-info-button button">X</button></div>
         </div>
     }
 }
