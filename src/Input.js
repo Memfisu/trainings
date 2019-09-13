a80a058157d6ed96aa55825de01d886d25c763de
GasAR <A.Gasymova@bssys.com>
2019-09-13 17:53:36 +0400

    ничего интересного

--- src/Input.js
+++ src/Input.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 class Input extends React.Component {
-    // state = {
-    //     value:"",
-    // }
-    //
-    // handleName = (event) => {
-    //     this.setState({value: event.target.value});
-    //     this.props.get(event.target.value);
-    // }
+    state = {
+        value:"",
+    }
+
+	handleName = (event) => {
+		this.setState({value: event.target.value});
+	}
 
     render() {
-        return <input type="text" className="input-list-item" /*onChange={this.handleName}*/ />
+        return <input type="text" className="input-list-item" />
     }
 }
 
