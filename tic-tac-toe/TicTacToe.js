14fcff7f49a87ba2e24b5e5bf0f3b587dcbe7e8e
Aigiul <memfiswho@gmail.com>
2019-10-09 00:06:36 +0400

    вроде почи всё

--- tic-tac-toe/TicTacToe.js
+++ tic-tac-toe/TicTacToe.js
@@ -7,9 +7,6 @@ class TicTacToe {
         ];
     }
 
-    //старт игры - инициализировать матрицу 3х3 со значениями null и как-то привязать координаты к инпутам
-    //новая игра - всё очистить
-
     getInput (x, y) { // получить инпут
         let input = document.getElementById(`input${x}${y}`);
         return input;
@@ -24,7 +21,7 @@ class TicTacToe {
         }
         
         input.disabled=true; // и заблокировать поле
-        computerTurn (); // и вызвать computerTurn ();
+        if (!this.isFinished()) {computerTurn ();} // проверить, не закончилась ли игра и вызвать computerTurn (), если нет
     }
 
     computerTurn () { // ход компьютера - поставить нолик на оставшееся свободное место, запомнить координаты
@@ -39,44 +36,55 @@ class TicTacToe {
                 };
             }
         }
+        this.isFinished();
     }
 
     isFinished() { //если игра окончена, возвращает победителя
-        let count_o = 0;
-        let count_x = 0;
-        for (let i=0; i<this.matrix.length; i++){  //если по диагонали-1 заполнено одинаковыми символами
-            for (let j=0; j<this.matrix.length; j++) {
-                if ((i=j)&&(matrix[i][j] = "o")) {count_o++;}
-                else if ((i=j)&&(matrix[i][j] = "x")) {count_x++;}
-            }
+        let array = [
+            [[0,0],[0,1],[0,2]],
+            [[1,0],[1,1],[1,2]],
+            [[2,0],[2,1],[2,2]],
+            [[0,0],[1,0],[2,0]],
+            [[0,1],[1,1],[2,1]],
+            [[0,2],[1,2],[2,2]],
+            [[0,0],[1,1],[2,2]],
+            [[0,2],[1,1],[2,0]]
+        ];
+        
+        for (let i=0; i<array.length; i++) {
+            let line = array[i];
+            let count_o = 0;
+            let count_x = 0;
+             for (let j=0; j<line.length; j++) {
+                let point = line[j];
+                let x = point[0];
+                let y = point[1];
+                if (this.matrix[x][y] = "o") {count_o++;}
+                else if (this.matrix[x][y] = "x") {count_x++;}
+             }
+            if (count_o===3) return "o"; // если есть строка с тремя ноликами
+            else if (count_x===3) return "x"; // если есть строка с тремя крестиками
         }
-        if (count_o===3) return "o";
-        else if (count_x===3) return "x";
-
-        count_o = 0;
-        count_x = 0;
-
-        for (let i=0; i<this.matrix.length; i++){  //если по диагонали-2 заполнено одинаковыми символами
-            for (let j=2; j>=0; j--) {
-                if (matrix[i][j] = "o") {count_o++;}
-                else if (matrix[i][j] = "x") {count_x++;}
-            }
-        }
-        if (count_o===3) return "o";
-        else (count_x===3) return "x";
 
         if (this.isDraw()) return "-"; // если ничья
-        return false;
+
+        getWinner(); //объявить победителя в любом из случаев
+
+        return false; // игра не окончена
     }
 
     getWinner() { 
-        // if(this.isFinished())
-        // return x, o, -
-        // иначе ничего не возвращать
-        // + в зависимости от победителя отобразить правильную надпись:
-        // p.classList.remove("end-game"); - при старте новой игры надпись про выигрыш скрывается
-        // p.classList.toggle("end-game"); - при выигрыше появляется (причём должны быть разные в зависимости от того, кто выиграл)
-        // p.classList.toggle("end-game"); - при ничьей появляется
+        if (this.isFinished()==="x") { // если игра окончена и победили крестики
+            document.getElementById("win").classList.remove("end-game"); // отобразить надпись о выигрыше (т.е. убрать скрытие надписи)
+        }
+        
+        if (this.isFinished()==="o") { // если игра окончена и победили нолики
+            document.getElementById("loose").classList.remove("end-game"); // отобразить надпись о проигрыше (т.е. убрать скрытие надписи)
+        }
+
+        if (this.isFinished()==="-") { // если игра окончена и ничья
+            document.getElementById("draw").classList.remove("end-game"); // отобразить надпись о ничьей (т.е. убрать скрытие надписи)
+        }
     }
 
     noMoreTurns() { // true, если всё поле уже заполнено
@@ -92,6 +100,24 @@ class TicTacToe {
     isDraw() { // true, если поле заполнено, а победителя нет
         if (this.noMoreTurns() && (this.getWinner()==="-")) return false;
         else return true;
+    }
+
+    restart() {  //новая игра - по клику на кнопку очистить матрицу + p.classList.toggle("end-game");
+        if (this.isFinished()==="x") {
+            document.getElementById("win").classList.toggle("end-game");
+        }
+        if (this.isFinished()==="o") {
+            document.getElementById("loose").classList.toggle("end-game");
+        }
+        if (this.isFinished()==="-") {
+            document.getElementById("draw").classList.toggle("end-game");
+        }
+
+        this.matrix = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
     }
 }
 
